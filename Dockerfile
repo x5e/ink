fe910fef52edbc6b6e97945d9f4d71a145651a46
Darin McGill <darin.mcgill@gmail.com>
2020-03-06 17:20:17 -0500

    added websocketpp code

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,8 @@ ENV LANG C.UTF-8
 ENV LC_ALL C.UTF-8
 ENV DEBIAN_FRONTEND noninteractive
 RUN apt-get install -y g++ cmake vim-tiny
-RUN apt-get install -y libmsgpack-dev libwebsockets-dev
+RUN apt-get install -y libmsgpack-dev
+RUN apt-get install -y libwebsocketpp-dev
 RUN mkdir -p /opt/x5e/ink
 WORKDIR /opt/x5e/ink
 COPY . .
--- main.cpp
+++ main.cpp
@@ -1,40 +1,10 @@
-#include <msgpack.hpp>
-#include <string>
-#include <iostream>
-#include <sstream>
+#include "main_msgpack.cpp"
+#include "main_websocketpp.cpp"
 
-int main()
-{
-    msgpack::type::tuple<int, bool, std::string> src(1, true, "example");
-
-    // serialize the object into the buffer.
-    // any classes that implements write(const char*,size_t) can be a buffer.
-    std::stringstream buffer;
-    msgpack::pack(buffer, src);
-
-    // send the buffer ...
-    buffer.seekg(0);
-
-    // deserialize the buffer into msgpack::object instance.
-    std::string str(buffer.str());
-
-    msgpack::object_handle oh =
-            msgpack::unpack(str.data(), str.size());
-
-    // deserialized object is valid during the msgpack::object_handle instance is alive.
-    msgpack::object deserialized = oh.get();
-
-    // msgpack::object supports ostream.
-    std::cout << deserialized << std::endl;
-
-    // convert msgpack::object instance into the original type.
-    // if the type is mismatched, it throws msgpack::type_error exception.
-    msgpack::type::tuple<int, bool, std::string> dst;
-    deserialized.convert(dst);
-
-    // or create the new instance
-    msgpack::type::tuple<int, bool, std::string> dst2 =
-                                            deserialized.as<msgpack::type::tuple<int, bool, std::string> >();
-
+int main(int argc, char** argv) {
+    if (argc > 1)
+        main_msgpack();
+    else
+        main_websocketpp();
     return 0;
 }
